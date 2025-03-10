fa9d3d408116e6a4f7e4c56bfd2e38bd3ba082c4
Ernest <ernestez12@gmail.com>
2025-03-10 11:50:20 +0100

    fix: fixed hero text

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -7,8 +7,8 @@ const HeroSlider = () => {
       {/* Your Content */}
       <div className="absolute inset-0 flex items-center justify-center z-20">
         <div className="text-center">
-          <h2 className="text-white text-tertiary uppercase mb-5 text-lg md:text-xl lg:text-4xl">Your One Spot</h2>
-          <h3 className="h3 m-auto text-sky-500 text-2xl md:text-3xl lg:text-6xl">TASK MANAGEMENT SOLUTION</h3>
+          <h2 className="text-white text-tertiary uppercase mb-5 text-lg md:text-xl lg:text-4xl">Your One-Stop</h2>
+          <h3 className=" font-bold m-auto text-sky-500 text-2xl md:text-3xl lg:text-6xl">TASK MANAGEMENT SOLUTION</h3>
         </div>
       </div>
     </div>
